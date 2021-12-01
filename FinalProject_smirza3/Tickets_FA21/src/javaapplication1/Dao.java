b393693c8b43f5db2038ee8a61b21fa7bd4e73e5
Shahaan Mirza <shahaanmirza@gmail.com>
2021-12-01 16:19:04 -0600

    Add start_date and end_date columns to table, and refactor createTables()

--- FinalProject_smirza3/Tickets_FA21/src/javaapplication1/Dao.java
+++ FinalProject_smirza3/Tickets_FA21/src/javaapplication1/Dao.java
@@ -39,8 +39,15 @@ public class Dao {
 
 	public void createTables() {
 		// variables for SQL Query table creations
-		final String createTicketsTable = "CREATE TABLE smirza3_tickets(ticket_id INT AUTO_INCREMENT PRIMARY KEY, ticket_issuer VARCHAR(30), ticket_description VARCHAR(200))";
-		final String createUsersTable = "CREATE TABLE smirza3_users(uid INT AUTO_INCREMENT PRIMARY KEY, uname VARCHAR(30), upass VARCHAR(30), admin int)";
+		final String createTicketsTable = "CREATE TABLE smirza3_tickets(ticket_id INT AUTO_INCREMENT PRIMARY KEY, " +
+				"ticket_issuer VARCHAR(30), " +
+				"ticket_description VARCHAR(200), " +
+				"start_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+				"end_date TIMESTAMP NULL)";
+		final String createUsersTable = "CREATE TABLE smirza3_users(uid INT AUTO_INCREMENT PRIMARY KEY, " +
+				"uname VARCHAR(30), " +
+				"upass VARCHAR(30), " +
+				"admin int)";
 
 		try {
 
